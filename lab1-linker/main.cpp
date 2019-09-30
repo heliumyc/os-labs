228fa29cd590c655f56ed46a08e153c761a8b896
heliumyc <ericyc4@gmail.com>
2019-09-30 16:14:30 -0400

    add pass_1()

--- lab1-linker/main.cpp
+++ lab1-linker/main.cpp
@@ -5,27 +5,28 @@
 #include "parser.h"
 #include "tokenizer.h"
 
+#define VERIFY_ELSE_BREAK(verification, error_statement) if(!verification) {error_statement;break;}
+
 using namespace std;
 
 vector<string> symbolList; // helper array to maintain the insertion order of hash table below
 unordered_map<string, int> symbolTable; // using hash table rather than rbt. string for symbol, int for absolute addr
 
-void pass1();
+void pass_1(istream& input, ostream& output) {
+    Tokenizer tokenizer(&input);
+    tokenizer.printAll();
+}
 
-void pass2();
+void pass_2(Tokenizer& tokenizer);
 
 int main(int argc, char *argv[]) {
     ifstream myFile;
-    myFile.open("test");
+    myFile.open("input-1");
 
-//    ofstream error;
-//    ostream error(cout);
-
-
-    Tokenizer tokenizer(&myFile);
-    tokenizer.printAll();
+    pass_1(myFile, cout);
 
     myFile.close();
+
 
     return 0;
 }
