e5786d0f1b6b800832e846a76c5989ab6da20d35
heliumyc <ericyc4@gmail.com>
2019-12-13 14:15:42 -0500

    bug fixed in flook

--- lab4-file/IO_Schedulers/FLOOK_IOScheduler.cpp
+++ lab4-file/IO_Schedulers/FLOOK_IOScheduler.cpp
@@ -16,7 +16,9 @@ void FLOOK_IOScheduler::AddNewIORequest(std::unique_ptr<Request> &&request) {
 
 void FLOOK_IOScheduler::FetchNext() {
     if (clockwise.empty() && counter_clockwise.empty()) {
+        // add queue is guaranteed to be non-empty, so after allocated, this will not be allocated again
         AllocateFromAddQueue();
+        return FetchNext();
     } else if (direction > 0 && clockwise.empty()) {
         active_queue = &counter_clockwise;
         direction = -1;
@@ -36,41 +38,41 @@ void FLOOK_IOScheduler::FetchNext() {
 
     active_io = std::move(*it);
     active_queue->erase(it);
-
     dest_track = active_io->track_num;
 }
 
 void FLOOK_IOScheduler::AllocateFromAddQueue() {
     // dissect the add queue!
-    std::deque<std::unique_ptr<Request>> temp;
-    int min_dist = INT32_MAX;
-    int min_op = INT32_MAX;
+//    std::deque<std::unique_ptr<Request>> temp;
+//    int min_dist = INT32_MAX;
+//    int min_op = INT32_MAX;
     while (!add_queue.empty()) {
         auto& it = add_queue.front();
-        int cur_dist = std::abs(it->track_num-head);
-        int cur_op = it->op_idx;
+//        int cur_dist = std::abs(it->track_num-head);
+//        int cur_op = it->op_idx;
         if (it->track_num > head) {
-            if (min_dist > cur_dist || (min_dist == cur_dist && min_op > cur_op)) {
-                min_dist = std::abs(it->track_num-head);
-                min_op = cur_op;
-                active_queue = &clockwise;
-            }
+//            if (min_dist > cur_dist || (min_dist == cur_dist && min_op > cur_op)) {
+//                min_dist = std::abs(it->track_num-head);
+//                min_op = cur_op;
+//                active_queue = &clockwise;
+//            }
             clockwise.push_back(std::move(it));
         } else if (it->track_num < head) {
-            if (min_dist > cur_dist || (min_dist == cur_dist && min_op > cur_op)) {
-                min_dist = std::abs(it->track_num-head);
-                min_op = cur_op;
-                active_queue = &counter_clockwise;
-            }
+//            if (min_dist > cur_dist || (min_dist == cur_dist && min_op > cur_op)) {
+//                min_dist = std::abs(it->track_num-head);
+//                min_op = cur_op;
+//                active_queue = &counter_clockwise;
+//            }
             counter_clockwise.push_back(std::move(it));
         } else {
-            temp.push_back(std::move(it));
+            active_queue->push_back(std::move(it));
+//            temp.push_back(std::move(it));
         }
         add_queue.pop_front();
     }
     // special case !!!
-    while (!temp.empty()) {
-        active_queue->push_back(std::move(temp.front()));
-        temp.pop_front();
-    }
+//    while (!temp.empty()) {
+//        active_queue->push_back(std::move(temp.front()));
+//        temp.pop_front();
+//    }
 }
